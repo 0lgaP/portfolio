2df787cdf0f2525153a56cdea0aec7b314637634
0lgatron <perronolga@gmail.com>
2022-04-18 16:15:44 -0400

    header styling

--- 0lga.ca/src/components/header/Header.jsx
+++ 0lga.ca/src/components/header/Header.jsx
@@ -3,6 +3,7 @@ import './header.css';
 import CTA from './CTA';
 import ME from '../../assets/0lga.png';
 import HeaderSocials from './HeaderSocials';
+import {BsArrowRightSquareFill} from 'react-icons/bs'
 
 
 const Header = () => {
@@ -17,9 +18,7 @@ const Header = () => {
       <div className='me'>
         <img src={ME} alt='Olga Perron' />
       </div>
-      <div>
       <a href='#contact' className='scroll__down'>Scroll Down</a>
-      </div>
       </div>
     </header>
   )
