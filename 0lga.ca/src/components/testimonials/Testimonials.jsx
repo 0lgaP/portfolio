3290815a731d0c1383a1ca0576dbe34abdb0cdce
0lgatron <perronolga@gmail.com>
2022-04-19 12:49:01 -0400

    testimonials started

--- 0lga.ca/src/components/testimonials/Testimonials.jsx
+++ 0lga.ca/src/components/testimonials/Testimonials.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import './testimonials.css';
-
+import Fem from '../../assets/blue.png'
+import Mal from '../../assets/purple.png'
 
 const Testimonials = () => {
   return (
-    <section id='testemonials'>Testemonials</section>  )
+    <section id='testemonials'>
+      <h5>Endorsemnts from collegues</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials__container">
+        <article class="testimonial">
+          <div className="client__avatar">
+            <img src={Fem} alt="name"/>
+            <h5 className='client__name'>Female Endorser</h5>
+            <small className='client__review'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione eaque, enim voluptate possimus neque vero, facilis ipsa dignissimos commodi rem odit impedit dolore quam ut harum autem error natus voluptas!</small>
+          </div>
+        </article>
+      </div>
+    </section>  )
 }
 
 export default Testimonials
