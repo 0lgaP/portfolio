952456c76d29d3e731c35ffe71eed5fbeed45686
0lgatron <perronolga@gmail.com>
2022-04-19 12:34:53 -0400

    add services information and portfolio - missing good portfolio images

--- 0lga.ca/src/components/portfolio/Portfolio.jsx
+++ 0lga.ca/src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,77 @@
 import React from 'react';
 import './portfolio.css';
+import Tweeter from '../../assets/t3.gif';
+import Jungle from '../../assets/jungle-sign-buy.gif';
+import PBM from '../../assets/pbm2.gif';
+import Scheduler from '../../assets/scheduler1.gif';
+import Dragon from '../../assets/StoryCardGif_01.gif';
+import {BsGithub} from 'react-icons/bs';
+
+const data = [
+  {
+    id: 1,
+    image: Tweeter,
+    title: 'Tweeter',
+    description: `Tweeter is a simple, single-page Twitter clone. Tweeter is responsive and adapts to your screen. This repository is created from the starter code provided by LHL. The layout, front-end functionality and animations were built using HTML, CSS, JS, jQuery and AJAX with a back end in Node and Express.`,
+    github: 'https://github.com/0lgaP/tweeter'
+  },
+  {
+    id: 2,
+    image: Jungle,
+    title: 'Jungle',
+    description: `Jungle is a mini e-commerce application built with Rails 4.2. This project simulates inheriting an existing code base in a language/framework that you are not comfortable/familiar with, then implementing features and fixes. Tools used are RJS, Stripe, PostgresSQL.`,
+    github: 'https://github.com/0lgaP/jungle-rails'
+  },
+  {
+    id: 3,
+    image: PBM,
+    title: 'Pokemon Black Market',
+    description: `This midterm project was a collaborative effort. My portion of this project focused on creating a dynamic home page with a filter function, search field and search result page. It utilises database queries to keep my EJS responsive, including alt text for accessibility, and dynamic links to connect to other parts of the page. To create the styling I used a combination of bootstrap and my own CSS. We created our own database based on an ERD we created to show relationships, and seeded  using a spreadsheet with formula to keep our data flexible. This project was written in EJS, Javascript and SCSS.`,
+    github: 'https://github.com/0lgaP/buy-sell-store-LHL'
+  },
+  {
+    id: 4,
+    image: Scheduler,
+    title: 'Scheduler',
+    description: `Scheduler is a single-page, React application. This user friendly app allows you to create, edit or delete interview. The database is managed by a separate API and requests are handled through Axios. The project comes with a testing suite utilising Jest and Cypress.`,
+    github: 'https://github.com/0lgaP/scheduler'
+  },
+  {
+    id: 5,
+    image: Dragon,
+    title: 'Dragon Drop',
+    description: `Dragon Drop is a multi-page React application that promotes content creation and world building.  The app accommodates a stream of consciousness workflow for the user - through automatic updates and persistent drop down selection.  All updates to the database are automatic and the user is empowered to switch between tabs  allowing for separation of concerns. They can re-order their story and map, create new characters and set a status. We focused on a non destructive design, where story data is added to campaign history, rather than deleted on completion. The API requests are handled through Axios, with  Node and Express back end. The front-end  layout uses tailwind, and the drag-and drop functionality uses the react-dnd library. I created the Dragon Drop logo, character icons, tavern map and assets using Inkscape.`,
+    github: 'https://github.com/0lgaP/Dragon-Drop'
+  }
+]
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>  )
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className='container portfolio__container'>
+        {
+          data.map(({id, image, title, description, github}) => {
+            return (
+              <article key={id} className='portfolio__item'>
+                <div className="portfolio__item-image">
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <small className='portfolio__item-desc'>{description}</small>
+                <div className='portfolio__item-cta'>
+                  <a href={github} className='btn btn-primary' target='_blank' rel="noreferrer"><BsGithub className='portfolio__item-icon'/> Code </a>
+                </div>
+            </article>
+            )
+          })
+        }
+        
+
+      </div>
+    </section>  )
 }
 
 export default Portfolio
--- 0lga.ca/src/components/services/Services.jsx
+++ 0lga.ca/src/components/services/Services.jsx
@@ -7,107 +7,82 @@ const Services = () => {
   return (
     <section id='services'>
       <h5>What I Offer</h5>
-      <h2>Services</h2>
+      <h2>Work History</h2>
 
       <div className="container services__container">
         <article className='service'>
           <div className="service__head">
-            <h3>UI/UX Design</h3>
+            <h3>Juno College of Technology</h3>
+            <small className='services__item-desc'>Administrative coordinator</small>
           </div>
 
           <ul className='service__list'>
             <li>
               <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet</p>
+              <p>Lead Tech-Talk preparation guiding students in best practices for stand-up presentation</p>
             </li>
             <li>
               <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet</p>
+              <p>Detail oriented data entry: transcript, grades, expense reports</p>
             </li>
             <li>
               <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet</p>
+              <p>Manage and set up Slack channels and Github invitations</p>
             </li>
             <li>
               <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet</p>
+              <p>Salesforce initiation of program folders and population of tracked content including formula for expected/current sales</p>
             </li>
             <li>
               <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet</p>
+              <p>Develop master docs for student tracking, course work, and grade tracking and link to master tracking using google sheets</p>
             </li>
             <li>
               <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet</p>
+              <p>Create full course packages including InDesign schedule, and digital distribution</p>
             </li>
           </ul>
         </article>
 
         <article className='service'>
           <div className="service__head">
-            <h3>Web Development</h3>
+            <h3>Design Exchange</h3>
+            <small className='services__item-desc'>Education coordinator</small>
           </div>
 
           <ul className='service__list'>
             <li>
               <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet</p>
+              <p>Speaker and educator for sustainable and accessible design principles</p>
             </li>
             <li>
               <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet</p>
+              <p>Public speaking commitments and award presentation for the Government of Ontario's Accessible Design Competition </p>
             </li>
             <li>
               <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet</p>
+              <p>Key note speaker and promoter on behalf of the Design Exchange programming team</p>
             </li>
             <li>
               <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet</p>
+              <p>Program Developer for camps and in-house design workshops</p>
             </li>
             <li>
               <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet</p>
+              <p>Delivered an increase in attendance through activity development and project management in the Home School Program</p>
             </li>
             <li>
               <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet</p>
+              <p>Website maintenance for the Expo Design Innovation and Technology (EDIT 2017)</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Content creation for tours, workshops and public programs</p>
             </li>
           </ul>
         </article>
 
-        <article className='service'>
-          <div className="service__head">
-            <h3>Content Creation</h3>
-          </div>
-
-          <ul className='service__list'>
-            <li>
-              <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet</p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet</p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet</p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet</p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet</p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet</p>
-            </li>
-          </ul>
-        </article>
+        
 
       </div>
     </section>  )
