cfa79c7f58b6bd0570d223036857da4ff01821a5
0lgatron <perronolga@gmail.com>
2022-06-21 10:31:02 -0400

    add lazy load

--- 0lga.ca/src/App.jsx
+++ 0lga.ca/src/App.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, {lazy, Suspense } from 'react';
 import Header from './components/header/Header';
 import Nav from './components/nav/Nav';
 import About from './components/about/About';
 import Experience from './components/experience/Experience';
 import Services from './components/services/Services';
-import Portfolio from './components/portfolio/Portfolio';
+// import Portfolio from './components/portfolio/Portfolio';
 import Testimonials from './components/testimonials/Testimonials';
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
 
 
 const App = () => {
+const Portfolio = lazy(()=>import('./components/portfolio/Portfolio'))
+
   return (
     <>
     <Header />
@@ -18,7 +20,9 @@ const App = () => {
     <About />
     <Experience />
     <Services />
+    <Suspense fallback={'...'}>
     <Portfolio />
+    </Suspense>
     <Testimonials />
     <Contact />
     <Footer />
